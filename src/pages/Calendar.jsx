6f4c1e2364c5c452eb862c5e033314a38b4337ab
wes <wes@hw>
2022-07-23 15:03:30 +0800

    添加调度事项

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import {ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, Inject} from '@syncfusion/ej2-react-schedule'
+
+import { scheduleData } from '../data/dummy'
 
 const Calendar = () => {
   return (
-    <div>Calendar</div>
+    <>
+      <ScheduleComponent
+        selectedDate={new Date(2021,0,10)}
+        height="650"
+        eventSettings={{dataSource: scheduleData}}
+      >
+        <Inject services={[Day, Week, WorkWeek, Month, Agenda]}/>
+      </ScheduleComponent>
+    </>
   )
 }
 
